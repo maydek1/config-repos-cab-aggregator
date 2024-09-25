3e69527b0731f4f65c51cfc4b787dff517bb3ab7
maydek1 <maydekcraft@gmail.com>
2024-09-25 13:15:44 +0300

    gateway: init module

---
+++ Eureka/src/test/java/com/software/modsen/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.software.modsen.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- Gateway/src/test/java/com/software/modsen/gateway/GatewayApplicationTests.java
+++ Gateway/src/test/java/com/software/modsen/gateway/GatewayApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.eureka;
+package com.software.modsen.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EurekaApplicationTests {
+class GatewayApplicationTests {
 
     @Test
     void contextLoads() {
